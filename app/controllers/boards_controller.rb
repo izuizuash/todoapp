80e797313a5140a5f623942304246eb1367d51e2
いず <maybe05.maggio@gmail.com>
2020-10-15 22:46:06 +0900

    記事の投稿

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -12,4 +12,19 @@ class BoardsController < ApplicationController
  def new
   @board = Board.new
  end
+
+ def create
+  @board = Board.new(board_params)
+  if @board.save
+    redirect_to board_path(@board)
+  else
+    render :new
+  end
+ end
+
+
+ private
+ def board_params
+  params.require(:board).permit(:title, :content)
+ end
 end
